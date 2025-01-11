4ff4dabfec22dbcd6754c62578715af68b9b3e58
Arjun Pramanik <arjun29.pramanik@gmail.com>
2025-01-11 01:24:29 -0500

    test signed and unsigned ints, update readme

--- 4.Integer Modifiers/main.cpp
+++ 4.Integer Modifiers/main.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 int main()
 {
-    auto result = (10 <=> 20) > 0;
-    cout << result << "\n";
+    signed int value0 = 10;
+    unsigned int value1 = 10;
+    signed int value2 = -10;
+    unsigned int value3 = -10;
+
+    cout << value0 << "\n";
+    cout << value1 << "\n";
+    cout << value2 << "\n";
+    cout << value3 << "\n";
+
+    return 0;
 }
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Projects seen here are based on freeCodeCamp's free C++ programming course!
+# Projects seen here are based on freeCodeCamp's free C++ programming course!
 
 Found at: [C++ Programming Course - Beginner to Advanced](https://youtu.be/8jLOx1hD3_o)
