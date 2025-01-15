a7feb4f223a6a83e5390ea2f83afc225527d3f14
Arjun Pramanik <arjun29.pramanik@gmail.com>
2025-01-14 21:54:25 -0500

    array of characters lesson

--- 7.3ArrayOfCharacters/main.cpp
+++ 7.3ArrayOfCharacters/main.cpp
@@ -4,21 +4,33 @@ using namespace std;
 int main()
 {
     // you DON'T need to use a loop to print out all the elements, char arrays enable printing out of all elements at once
-    char message1[] {'H', 'e', 'l', 'l', 'o'};
+    char message1[] = {'H', 'e', 'l', 'l', 'o'};
     // instead of just printing out "Hello", a bunch of random characters will be appended
     cout << "message[]: " << message1 << "\n";
+    cout << "message[] element count: " << size(message1) << "\n\n";
 
     // null terminator must be added to signify the end of the char array
-    char message2[] {'H', 'e', 'l', 'l', 'o', '\0'};
+    char message2[] = {'H', 'e', 'l', 'l', 'o', '\0'};
     cout << "message[] WITH null terminator: " << message2 << "\n";
-    
     // number of elements includes the null terminator
-    cout << "message[] element count: " << size(message2);
+    cout << "message[] element count: " << size(message2) << "\n\n";
 
-    // initializing array with auto-filled null chars (size 6 but 5 chars given)
-    char message3[6] {'H', 'e', 'l', 'l', 'o'};
-    cout << "message[] WITH auto-filled: " << message3 << "\n";
-    cout << "message[] element count: " << size(message3);
+    // initializing array with an extra element AUTO-fills in the null
+    // ...terminator
+    char message3[6] = {'H', 'e', 'l', 'l', 'o'};
+    cout << "message[] WITH auto-filled null terminator: " << message3 << "\n";
+    cout << "message[] element count: " << size(message3) <<"\n\n";
+
+    // defining a literal C string is allowed
+    char message4[] = {"Hello"}; // implicit \0 is appendeded
+    // ...making it a C string
+    cout << "message[] WITH literal C string: " << message4 << "\n";
+    cout << "message[] element count: " << size(message4) <<"\n\n";
+
+    // spaces are valid as well
+    char message5[] = {"Hello World!"};
+    cout << "message[] WITH literal C string with spaces: " << message5 << "\n";
+    cout << "message[] element count: " << size(message5) <<"\n\n";
 
     return 0;
 }
